4711de1fd9181b50a611f201de9ccea2fffc1103
Jesse Merida <36455659+Aperture98@users.noreply.github.com>
2018-09-08 02:12:46 -0400

    Menu UI w/o Art

---
+++ Assets/Script/UIQuit.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIQuit : MonoBehaviour {
+    public void quitUI() {
+        Application.Quit();
+        Debug.Log("Quit");
+       }
+}
