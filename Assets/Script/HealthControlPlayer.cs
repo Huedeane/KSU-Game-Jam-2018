3183e3f7e777d2bdbeab95f8777f3806835b3996
Jesse Merida <36455659+Aperture98@users.noreply.github.com>
2018-09-09 04:48:02 -0400

    Made the player healthbar

---
+++ Assets/Script/HealthControlPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthControlPlayer : MonoBehaviour {
+     public Image bar;
+     public float fill;
+
+     public void Start()
+     {
+          fill = 1f;
+     }
+
+     public void Update()
+     {
+          bar.fillAmount = fill;
+     }
+}
